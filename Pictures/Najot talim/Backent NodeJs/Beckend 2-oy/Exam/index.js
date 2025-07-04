3da0ffec4b5c44eccc196d19e78247583ec20fb3
elyor595 <elyrbekbakhtiyarov5.@gmail>
2025-07-04 18:37:42 +0500

    exam 2-month

---
+++ Pictures/Najot talim/Backent NodeJs/Beckend 2-oy/Exam/index.js
@@ -0,0 +1,82 @@
+// 1-misol
+// function countBy(x, n) {
+//   let newArr = [];
+//   for (let i = 1; i <= n; i++) {
+//     newArr.push(x * i);
+//   }
+//   return newArr;
+// }
+// console.log(countBy(1,10));
+
+
+// 2-misol
+// function myFunc(arr){
+//     let newArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 ==0) {
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
+// console.log(myFunc([1,22,31,4,15,6,70,8]));
+
+// 3-misol
+// function getSum(arr) {
+//   let result = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       result += arr[i][j];
+//     }
+//   }
+//   return result
+// }
+
+// console.log(getSum([
+//   [1, 0, 0, 0, 0],
+//   [0, 1, 0, 0, 0],
+//   [0, 0, 1, 0, 0],
+//   [0, 0, 0, 1, 0],
+//   [0, 0, 0, 1, 5],
+// ]));
+
+
+
+// 4-misol
+// function maxNumber(arr) {
+//     let result = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > result) {
+//             result = arr[i];
+//         }
+//     }
+//     return result
+// }
+// console.log(maxNumber([22, 55, 33, 130, 11, 66, 123]));
+
+
+// 5-misol
+// function myFunc(arr){
+//     let newArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 3 ==0 || arr[i] % 5 ==0) {
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
+// console.log(myFunc([1,3,5,10,15,20,22]));
+
+
+// 6-misol
+// function upper(arr) {
+//     const newArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         newArr.push(arr[i].toUpperCase())
+//     }
+//     return newArr;
+// }
+
+// console.log(upper(['salom', 'nima', 'gap'])); 
+
+
